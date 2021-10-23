bb4e1ea9c794e07a268acc0fcd3e06ddc2275ad8
Ravi Mohan <ravi.mohan@roonga.com.au>
2021-10-24 10:35:53 +1100

    additional config

--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,55 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	let defaultZoomLevel = vscode.workspace.getConfiguration('').get('window.zoomLevel');
+	const zoomLevel = 'zoomLevel';
+	const fullScreen = 'fullScreen';
+	const centeredLayout = 'centeredLayout';
+	const showSideBar = 'showSideBar';
+	const showStatusBar = 'showStatusBar';
+	const showActivityBar = 'showActivityBar';
+	const showPanel = 'showPanel';
+	const showEditorArea = 'showEditorArea';
+	const sideBarLocation = 'sideBarLocation';
+	const panelLocation = 'panelLocation';
+
+	let defaultZoomLevel = vscode.workspace.getConfiguration('').get(`window.${zoomLevel}`);
+	let defaultFullScreen = vscode.workspace.getConfiguration('').get(`window.${fullScreen}`);
+	let defaultCenteredLayout = vscode.workspace.getConfiguration('').get(`window.${centeredLayout}`);
+	let defaultShowSideBar = vscode.workspace.getConfiguration('').get(`window.${showSideBar}`);
+	let defaultShowStatusBar = vscode.workspace.getConfiguration('').get(`window.${showStatusBar}`);
+	let defaultShowActivityBar = vscode.workspace.getConfiguration('').get(`window.${showActivityBar}`);
+	let defaultShowPanel = vscode.workspace.getConfiguration('').get(`window.${showPanel}`);
+	let defaultShowEditorArea = vscode.workspace.getConfiguration('').get(`window.${showEditorArea}`);
+	let defaultSideBarLocation = vscode.workspace.getConfiguration('').get(`window.${sideBarLocation}`);
+	let defaultPanelLocation = vscode.workspace.getConfiguration('').get(`window.${panelLocation}`);
 
 	let startDemo = vscode.commands.registerCommand('start-demo.startstartDemo', () => {
 		vscode.commands.executeCommand('setContext', 'instartDemo', true);
-		setZoomLevel();
+		set(fullScreen);
+		set(centeredLayout);
+		set(showSideBar);
+		set(showStatusBar);
+		set(showActivityBar);
+		set(showPanel);
+		set(showEditorArea);
+		set(sideBarLocation);
+		set(panelLocation);
+		set(zoomLevel);
 		setZenMode();
 	});
 
 	let endDemo = vscode.commands.registerCommand('start-demo.endstartDemo', () => {
 		vscode.commands.executeCommand('setContext', 'instartDemo', false);
-		resetZoomLevel(defaultZoomLevel);
+		reset(fullScreen, defaultFullScreen);
+		reset(centeredLayout, defaultCenteredLayout);
+		reset(showSideBar, defaultShowSideBar);
+		reset(showStatusBar, defaultShowStatusBar);
+		reset(showActivityBar, defaultShowActivityBar);
+		reset(showPanel, defaultShowPanel);
+		reset(showEditorArea, defaultShowEditorArea);
+		reset(sideBarLocation, defaultSideBarLocation);
+		reset(panelLocation, defaultPanelLocation);
+		reset(zoomLevel, defaultZoomLevel);
 		resetZenMode();
 	});
 
@@ -20,13 +58,13 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(endDemo);
 }
 
-function setZoomLevel() {
-	let zoomLevelInstartDemo = vscode.workspace.getConfiguration('').get('startDemo.zoomLevel');
-	vscode.workspace.getConfiguration('').update('window.zoomLevel', zoomLevelInstartDemo, true);
+function set(section: string) {
+	let config = vscode.workspace.getConfiguration('').get(`startDemo.${section}`);
+	vscode.workspace.getConfiguration('').update(`window.${section}`, config, true);
 }
 
-function resetZoomLevel(defaultZoomLevel: any) {
-	vscode.workspace.getConfiguration('').update('window.zoomLevel', defaultZoomLevel, true);
+function reset(section: string, defaultValue: any) {
+	vscode.workspace.getConfiguration('').update(`window.${section}`, defaultValue, true);
 }
 
 function setZenMode() {
@@ -45,5 +83,4 @@ function resetZenMode() {
 
 // this method is called when your extension is deactivated
 export function deactivate() {
-
 }
